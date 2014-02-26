6f675024fea21ab6a2a9c69e0315657b5cb906df
Belen Armas <alu0100696677@ull.edu.es>
2014-02-26 10:28:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SegundoProyecto::Application.config.secret_key_base = '4f52c3a26d69d83f9ff4e3a2ecd9db0072e2b6df1416861170bbb9cf78292303fec9e2bf1c3477586c680bbe29aed73f1cb63470531c9c556c906621ed32c9b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SegundoProyecto::Application.config.session_store :cookie_store, key: '_segundo_proyecto_session'
